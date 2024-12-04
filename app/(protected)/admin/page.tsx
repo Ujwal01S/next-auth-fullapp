cc76754a3296eea7924a9a21ae51af94828e32fc
Ujwal Suwal <snow7neanderthal477@gmail.com>
2024-12-04 12:32:35 +0545

    Redeploy

--- app/(protected)/admin/page.tsx
+++ app/(protected)/admin/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 
 import { admin } from "@/actions/admin";
-import { RoleGate } from "@/components/auth/role-gate";
+
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { FormSuccess } from "@/components/ui/form-success";
-import { UserRole } from "@prisma/client";
+
+
 import { toast } from "sonner";
 
 const AminPage = () => {
@@ -36,10 +36,7 @@ const AminPage = () => {
         <p className="text-2xl font-semibold text-center">Admin</p>
       </CardHeader>
       <CardContent className="space-y-4">
-        <RoleGate allowedRole={UserRole.ADMIN}>
-          {/* <FormSuccess message="You are allowed to see this content" /> */}
-          <></>
-        </RoleGate>
+
 
         <div className="flex flex-row items-center justify-between rounded-lg border p-3 shadow-md">
           <p className="text-sm font-medium">Admin-only API Route</p>
